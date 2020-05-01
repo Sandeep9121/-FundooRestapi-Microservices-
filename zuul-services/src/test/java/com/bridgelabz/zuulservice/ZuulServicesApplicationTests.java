9a6cabe821e73dc8dcc0e2f6b989b8d99251db59
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-05-01 09:54:04 +0100

    [Sandeep].add zuul service to User-Services port is added in the zuul

---
+++ zuul-services/src/test/java/com/bridgelabz/zuulservice/ZuulServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.zuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
